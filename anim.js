8c4b370a9c187792b1de2cf5df6dc013ececa299
Joel Gonçalves <joelkunguromix@gmail.com>
2023-01-19 23:51:00 +0000

    html + js

--- anim.js
+++ anim.js
@@ -1,10 +1,9 @@
-document.querySelector(function () {
+$(function(){
 
-    if (!document.on('.envelope').classList.contains('open')) {
-        document.on('.envelope').click(function () {
-            document.on(this).classList.remove('new').classList.add('open');
-        });
+    if (!$('.envelope').hasClass('open')){
+      $('.envelope').click(function(){
+        $(this).removeClass('new').addClass('open');
+      });
     }
-
-});
-
+    
+  });
